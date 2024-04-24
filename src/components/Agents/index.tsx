24f43a9bb436608e6b02a27ebbcecd8dee59d7e7
Marcus Daniel <127118676+Daniel777-hub@users.noreply.github.com>
2024-04-24 16:15:09 -0300

    first commit

---
+++ src/components/Agents/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import styles from './styles.module.scss'
+
+interface AgentProps {
+  agent: {
+    role: {
+      displayName: string
+    }
+    displayName: string
+    abilities: {
+      displayIcon: string
+      displayName: string
+    }[]
+    fullPortrait: string
+  }
+}
+
+export const Agent = ({ agent }: AgentProps) => (
+  <a href="#" className={styles.agent}>
+  <div className={styles.text}>
+    <p>{agent.role.displayName}</p>
+  <strong>{agent.displayName}</strong>
+  </div>
+
+  <ul className={styles.abilities}>
+    {agent.abilities.map(ability => (
+          <li key={ability.displayName}>
+          <Image
+            src={ability.displayIcon}
+            width={36}
+            height={36}
+            alt={'Icone da habilidade' + ability.displayName}
+            />
+          </li>
+    ))}
+    </ul>
+
+    <div className={styles.background}>
+      <span style={{backgroundImage: `url('${agent.fullPortrait}')`}} />
+    </div>
+  </a>
+)
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from 'components/Header'
+import { Main } from 'components/Main'
+import styles from 'styles/home.module.scss'
+
+const HomePage = () => (
+  <div className={styles.container}>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default HomePage
