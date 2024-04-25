2eb17bda163529632a87d69ab7003e346ded8878
Marcus Daniel <127118676+Daniel777-hub@users.noreply.github.com>
2024-04-25 13:38:57 -0300

    integração com a API

---
+++ src/components/CardAgent/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import type { Agent } from 'types/agent';
+
+import styles from './styles.module.scss';
+
+export const CardAgent = (agent: AgentProps) => {
+  const renderAbilities = agent.abilities.map(ability => {
+    if (ability.displayIcon) {
+      return (
+        <li key={ability.displayName}>
+          <Image
+            src={ability.displayIcon}
+            width={36}
+            height={36}
+            alt={'Ícone da habilidade ' + ability.displayName}
+          />
+        </li>
+      );
+    }
+  });
+
+  return (
+    <a href="#" className={styles.agent}>
+      <div className={styles.text}>
+        <p>{agent.role.displayName}</p>
+        <strong>{agent.displayName}</strong>
+      </div>
+
+      <ul className={styles.abilities}>
+        {renderAbilities}
+      </ul>
+
+      <div className={styles.background}>
+        <span style={{backgroundImage: `url('${agent.fullPortrait}')`}} />
+      </div>
+    </a>
+  );
+};
